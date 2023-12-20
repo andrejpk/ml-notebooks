9462597d4e1e1eb74823a90c66714cf9c2da904a
Andrej Kyselica <andrejky@microsoft.com>
2023-12-20 16:58:44 -0500

    Move to agent for chat

--- secAgentChat.py
+++ secAgentChat.py
@@ -1,21 +1,36 @@
-import os
-from llama_index import OpenAIEmbedding, StorageContext, VectorStoreIndex, ServiceContext, load_index_from_storage
-from llama_index.text_splitter import SentenceSplitter
-from SecFilingReader import SecFilingReader
-from llama_index.ingestion import IngestionPipeline
-from llama_index.text_splitter import TokenTextSplitter
-from llama_index.extractors import TitleExtractor, QuestionsAnsweredExtractor
 import dotenv
 import logging
 import sys
 
+from llama_index.llms import OpenAI
+from llama_index.agent import OpenAIAgent
+from llama_index.agent import ReActAgent
+
+from secIndex import get_edgar_index
 from utils import get_env_var
+from langchain.agents import initialize_agent
+# from llama_index.langchain_helpers.agents import IndexToolConfig, LlamaIndexTool, LlamaToolkit, create_llama_agent
+# from llama_index.langchain_helpers.memory_wrapper import GPTIndexChatMemory
+from llama_index.tools import QueryEngineTool, ToolMetadata
+
+def create_filing_tool(cik: str, company_name: str):
+	index = get_edgar_index(cik)
+	print(f"Loaded index and tool for {company_name}")
+
+	# create the chat engine
+	company_filing_tool = QueryEngineTool(
+		query_engine=index.as_query_engine(),
+		metadata=ToolMetadata(
+			name= f"company_filing_tool_{company_name}",
+			description= "A tool for querying {company_name} SEC filings"
+		),
+	)
+	return company_filing_tool
 
 if __name__ == "__main__":
 	# Set up env vars
 	dotenv.load_dotenv()
 	open_api_key = get_env_var('OPENAI_API_KEY')
-	user_email = get_env_var('USER_EMAIL')
 
 	logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 	logging.getLogger("pyrate_limiter").setLevel(logging.WARNING)
@@ -23,53 +38,13 @@ if __name__ == "__main__":
 	logging.getLogger().handlers.clear()
 	logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
-	storage_path = "./storage"
-	if not os.path.exists(storage_path):
-		# load the documents and create the index
-		print("Creating index")
+	msft_tool = create_filing_tool("0000789019", "Microsoft")
+	amzn_tool = create_filing_tool("0001018724", "Amazon")
 
-		logging.debug("Loading Documents")
-		documents = SecFilingReader(user_email=user_email).load_data(cik="0000789019", filing_date_min="2020-01-01", forms=["10-K"])
-		logging.debug(f"  Documents: {len(documents)}")
+	llm = OpenAI(model="gpt-3.5-turbo-0613")
+	agent = ReActAgent.from_tools([msft_tool, amzn_tool], llm=llm, verbose=True)
 
-		transformations = [
-			SentenceSplitter(),
-			TitleExtractor(),
-			OpenAIEmbedding()
-			]
-
-		pipeline = IngestionPipeline(
-			transformations=transformations,	
-		)
-		logging.debug("Documents -> Nodes")
-		nodes = pipeline.run(documents=documents)
-		logging.debug(f"  Nodes: {len(nodes)}")
-
-		logging.debug("Nodes -> Index")
-		index = VectorStoreIndex(nodes)
-		# index = VectorStoreIndex.from_documents(documents, transformations=transformations)
-
-		# store it for later
-		print("Storing index")
-		index.storage_context.persist(persist_dir=storage_path)
-	else:
-		# load the existing index
-		storage_context = StorageContext.from_defaults(persist_dir=storage_path)
-		index = load_index_from_storage(storage_context)
-		print("Loaded index from storage")
-
-
-	# create the chat engine
-	engine = index.as_chat_engine()
-
-	print("Welcome to the SEC Chatbot. Type 'exit' to quit.")
-	while True:
-		query = input("Enter query: ")
-		if (query == "exit"):
-			break
-		results = engine.chat(query)
-		print(results)
-		# print(results[0].document.text)
-
-
+	
+	print(f"Ask me questions about Microsoft and Amazon!")
+	agent.chat_repl()
 
